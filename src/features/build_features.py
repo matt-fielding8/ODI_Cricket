e707f8df8ad61dddd45d26b79dee3f5ad04d49d4
Matthew Fielding <mattfielding8@gmail.com>
2019-07-27 19:54:45 +0100

    Feat: adds yearPageLinks function
    yearPageLinks returns all relative links belonging to:
    class_ = "QuoteSummary". Raises exception if class_ doesn't exist within
    the soup.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -97,7 +97,8 @@ def checkMerge(df, col_list1, col_list2):
         except:
             print("AssertionError:", col_list1[i], "!=", col_list2[i])
 
-# Gather missing match data
+## Script for gathering data
+#Gather missing match data
 def getScoreData(url):
     ''' str -> dct
     Uses requests and bs4 libraries to extract and parse html data from url.
@@ -125,3 +126,17 @@ def getSoup(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
     return soup
+
+def yearPageLinks(soup):
+    ''' wb -> list of str
+    Extracts relative links in "QuoteSummary" class from soup.
+    Returns relative url's as a list of str.
+    '''
+    link_list = []
+    try:
+        for i in soup.find_all(class_='QuoteSummary'):
+            link_list.append(i['href'])
+    except:
+        print('Class "QuoteSummary" does not exist')
+
+    return link_list
