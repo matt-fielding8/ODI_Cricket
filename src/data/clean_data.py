3800945e2f9c67e30e5306dbf1ba227abe6743a4
Matthew Fielding <mattfielding8@gmail.com>
2019-08-13 15:46:08 +0100

    Feat: adds replaceNaN feature to clean_data module
    replaceNaN replaces a given string value with np.NaN for a given list
    of columns.

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -3,6 +3,7 @@ All scripts for cleaning ODI data
 '''
 
 import pandas as pd
+import numpy as np
 
 def formatHeaders(df):
     '''
@@ -17,3 +18,10 @@ def renameCols(df, old_name_list, new_name_list):
     '''
     for i in range(len(old_name_list)):
         df.rename(columns={old_name_list[i]:new_name_list[i]}, inplace=True)
+
+def replaceNaN (df, col_list, str):
+    ''' (df, list of str, str) -> df
+    Replace values equal to str in df[col_list] with NaN.
+    '''
+    for col in col_list:
+        df[col] = np.where(df[col] == str, np.NaN, df[col])
