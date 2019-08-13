eb8424452e9a41e860d1257b7b80726ee770f5d2
Matthew Fielding <mattfielding8@gmail.com>
2019-08-13 15:42:19 +0100

    Feat: add renameCols feature to clean_data module
    renameCols takes a list of old column header names and replaces with a
    list of new column header names.

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -9,3 +9,11 @@ def formatHeaders(df):
     Rename all column headers lower case with whitespace removed.
     '''
     df.rename(lambda c: c.lower().replace(' ', '_'), axis=1, inplace=True)
+
+def renameCols(df, old_name_list, new_name_list):
+    '''
+    Replace columns in old_name_list with names in new_name_list.
+    Pre-condition: len(old_name_list) == len(new_name_list)
+    '''
+    for i in range(len(old_name_list)):
+        df.rename(columns={old_name_list[i]:new_name_list[i]}, inplace=True)
