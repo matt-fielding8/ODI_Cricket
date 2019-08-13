74de27aa6cd2de5a2629ff2ac885b7d3d6b9eee5
Matthew Fielding <mattfielding8@gmail.com>
2019-08-13 15:39:38 +0100

    Feat: adds formatHeaders to clean_data module
    formatHeaders makes all column heads lower case with whitespace replaced
    with "_"

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,3 +1,11 @@
 '''
 All scripts for cleaning ODI data
 '''
+
+import pandas as pd
+
+def formatHeaders(df):
+    '''
+    Rename all column headers lower case with whitespace removed.
+    '''
+    df.rename(lambda c: c.lower().replace(' ', '_'), axis=1, inplace=True)
